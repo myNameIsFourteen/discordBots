69664d67c902903940da46cd791d443c941503b4
Mike Anastasia <mynameisfourteen@gmail.com>
2020-06-12 21:41:54 -0400

    Added !startPlayer command.

--- src/main/java/bot/DrafterBot.java
+++ src/main/java/bot/DrafterBot.java
@@ -13,6 +13,7 @@ import qe.QeManager;
 import javax.annotation.Nonnull;
 import javax.security.auth.login.LoginException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -41,7 +42,20 @@ public class DrafterBot extends ListenerAdapter {
         Message message = event.getMessage();
         String content = message.getContentRaw();
 
-        if (content.startsWith("!1846draft")) {
+        if (content.startsWith("!startPlayer")) {
+            if (event.getChannelType() == ChannelType.TEXT) {
+                StringBuilder bldr = new StringBuilder();
+                List<Member> users = new ArrayList<>(event.getMessage().getMentionedMembers());
+                Collections.shuffle(users);
+                int i = 1;
+                for (Member user : users) {
+                    bldr.append(user.getEffectiveName() + " is in seat " + (i++) + "\n");
+                }
+                if (bldr.length() > 0) {
+                    event.getChannel().sendMessage(bldr.toString()).queue();
+                }
+            }
+        } else if (content.startsWith("!1846draft")) {
             if (currentGame == null) {
                 currentGame = new Publisher46(event, () -> {currentGame = null;});
             } else {
@@ -58,7 +72,8 @@ public class DrafterBot extends ListenerAdapter {
                 if (event.getMessage().getMentionedMembers() != null) {
                     for (Member member : event.getMessage().getMentionedMembers()) {
                         if (member.getUser().getId().equals(selfUser.getId())) {
-                            event.getChannel().sendMessage("Did somebody say my name?\nIf you would like to start a game of 1846, send a message that starts with \"!1846draft\".\n Make sure to @mention each of the players in the game in that message.").complete();
+                            event.getChannel().sendMessage("Did somebody say my name?\nIf you would like to start a game of 1846, send a message that starts with \"!1846draft\".\n Make sure to @mention each of the players in the game in that message.\n"
+                            + "Alternatively, you can use \"!startPlayer\" and @mention any number of players to generate a random seating order.").complete();
                             return;
                         }
                     }
