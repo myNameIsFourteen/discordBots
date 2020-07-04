a0a7f326f25ed73c32be6427cc011b14217833b2
Mike Anastasia <mynameisfourteen@gmail.com>
2020-07-04 14:32:56 -0400

    Public, and java 11.

--- src/main/java/bot/HelloWorld.java
+++ src/main/java/bot/HelloWorld.java
@@ -4,7 +4,7 @@ package bot;
  * Created by micha on 7/3/2020.
  */
 public class HelloWorld {
-    static void main(String[] args){
+    public static void main(String[] args){
         System.out.println("Hola mundi XIV");
     }
 }
